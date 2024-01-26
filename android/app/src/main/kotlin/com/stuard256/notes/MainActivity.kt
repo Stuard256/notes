513c223a86ed4b2e568e5be8af2bbf90b7e38b33
Андрей Муха <Stuard256@gmail.com>
2024-01-26 14:29:43 +0300

    create design placeholder note & add font

--- android/app/src/main/kotlin/com/stuard256/notes/MainActivity.kt
+++ android/app/src/main/kotlin/com/stuard256/notes/MainActivity.kt
@@ -2,5 +2,4 @@ package com.stuard256.notes
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'custom_app_widget.dart';
+import 'custom_note_item.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
+
