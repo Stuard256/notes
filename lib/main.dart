88f8e4920c4d1d2e37d388289923af2c7d49a5a9
Андрей Муха <Stuard256@gmail.com>
2024-01-28 00:22:30 +0300

    create cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes/constants.dart';
 import 'package:notes/views/notes_view.dart';
